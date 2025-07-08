aaaf6aab86bde88d7f3efaecb8d1eb120e8010a7
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-08 14:42:19 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-git pull origin main #repo updating in terminal
-pip install -r requirements.txt #installing modules
+git pull origin main 
+pip install -r requirements.txt 
 python hello.py
 python qrgen.py
 pause
