58c79c309871fe47bbcb6a606e4a80fec744f202
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-08 14:59:16 +0530

    Update qrgen.py

--- qrgen.py
+++ qrgen.py
@@ -1,5 +1,5 @@
 import qrcode
-
-img = qrcode.make("https://github.com/akshaydev")
-img.save("my_qr.png")
-print("QR code saved as my_qr.png")
+def qrgen():
+  img = qrcode.make("https://github.com/akshaydev")
+  img.save("my_qr.png")
+  print("QR code saved as my_qr.png")
