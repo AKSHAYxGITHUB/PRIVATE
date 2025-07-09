83717693b3d6c1d5a8693272f9358e0bf1e44fee
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-09 09:28:37 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -2,4 +2,4 @@ import pywhatkit as pwk
 def whatsapp():
   phone_number=input("Enter the phone number (only suuports indian numbers) :")
   message=input("Enter the message you whant to send :")
-  pwk.sendwhatmsg("+91"+phone_number,message,wait_time=10, tab_close=True)
+ pwk.sendwhatmsg_instantly("+91"+phone_number, message, wait_time=10, tab_close=True)
