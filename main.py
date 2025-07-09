092cffb4ebd80211a63142aad6e045f6b52b13de
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-09 12:19:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def run():
   init(autoreset=True)
   print(Fore.GREEN +"="*87)
   print(Fore.YELLOW + "Select the program that you want to run :" + Style.RESET_ALL)
-  print(Fore.CYAN + "  1)" + Fore.GREEN + " Hello print")
+  print(Fore.CYAN + "  1)" + Fore.GREEN + "Print any thing !")
   print(Fore.CYAN + "  2)" + Fore.GREEN + " Qr generator")
   print(Fore.CYAN + "  3)" + Fore.GREEN + " Whatsapp Message Sender")
   print(Fore.CYAN + "  e)" + Fore.RED + " exit")
