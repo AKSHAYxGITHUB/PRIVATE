4d5a880452fa23b833ec2162fbd15553561ed206
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-17 10:15:43 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+print("Welcome to Akshay's CHAT BOT ! ")
+  i=input("user :")
+def database(i):
+  i=input("user :")
+  if "hi" in i.lower():
+    print("HELLO !")
+    database()
+  elif "name" in i.lower():
+    print("MY NAME IS CHAT BOT !")
+    database()
+  else:
+    database()
+  
