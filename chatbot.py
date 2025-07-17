c2e3032e47d84af63fca82872bc713a286104db2
Akshay P P <105354161+AKSHAYxGITHUB@users.noreply.github.com>
2025-07-17 10:29:47 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,29 @@
+import qrgen,whatsapp
 def chatbot():
   print("Welcome to Akshay's CHAT BOT ! ")
   database()
   
 def database():
-  i=input("user :")
-  if "hi" in i.lower():
-    print("HELLO !")
+  i=input("user :").lower()
+  if "hi" in i:
+    print("BOT: HELLO !")
     database()
-  elif "name" in i.lower():
-    print("MY NAME IS CHAT BOT !")
+  elif "name" in i:
+    print("BOT: MY NAME IS CHAT BOT !")
+    database()
+  elif "whatsapp" in i:
+    whatsapp.whatsapp()
+    database()
+  elif "qrgen" in i:
+    qrgen.qrgen()
+    database()
+  elif "help" in i:
+    print("""
+          I AM A SIMPLE (BETA) CHAT BOT 
+          I CAN HELP YOU WITH MY LIMTED POW
+          If you need : [qrcode generator type "qrgen"]
+                        [whatsapp message type "whatsapp"]
+           """
     database()
   else:
     database()
